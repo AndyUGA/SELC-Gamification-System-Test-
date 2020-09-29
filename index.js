8155cb0f66993e01f8434d97d4e31f74fa25be30
Andy Truong <att35663@uga.edu>
2020-09-29 14:34:41 -0400

    Added form and updated backend

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.set("view engine", "ejs");
 
 
 client.connect(err => {
-    const selcCollection = client.db("test").collection("SELC");
+    const selcCollection = client.db("test").collection("selcAttendeeData");
     const selcHistory = client.db("test").collection("SELCHistory");
 
 
@@ -30,12 +30,88 @@ client.connect(err => {
     app.get('/', (req, res) => {
 
 
+        
         res.render("home", {
           title: "SELC"
         });
     })
 
+    app.get('/test', (req, res) => {
+        selcCollection.find({}).sort().toArray(function (err, result) {
+
+            console.log(42, result);
+            res.render("test", {
+                title: "School",
+                result: result
+            });
+        });
+    });
     /*
+       //Submit points into system
+    app.post('/increasePoints', (req, res) => {
+
+        const personID = req.params.id;
+        const points = parseInt(req.body.points);
+        const teamName = req.body.teamName;
+
+        const name = req.body.player;
+        const game = req.body.game;
+
+
+    
+        const teamIdentifier = {
+            name: teamName
+        };
+
+
+        let teamPoints = {
+            $inc: {
+                points: points
+            }
+        }
+
+        let tempDate = new Date().toString();
+
+        let finalDate = tempDate.substring(0, 25);
+
+        let history = {
+            name: name,
+            game: game,
+            points: points,
+            team: teamName,
+            date: finalDate,
+        }
+
+
+        collection.updateOne(teamIdentifier, teamPoints, (err, item) => {
+            if (err) {
+                res.send({ "104 Error is ": + err });
+            }
+            else {
+
+                console.log("Added " + points + " points to " + teamName);
+
+            }
+        });
+        element3History.insertOne(history, (err, item) => {
+            if (err) {
+                console.log(131, err);
+                res.send({ "131 Error is ": + err });
+            }
+            else {
+                console.log("Saved history successfully");
+            }
+        });
+
+
+
+
+
+        res.redirect("/");
+    });
+
+
+
     //Save history into system 
     app.post('/saveHistory', (req, res) => {
 
@@ -49,7 +125,7 @@ client.connect(err => {
         }
 
         console.log(240, history);
-        element3History.insertOne(history, (err, item) => {
+        selcHistory.insertOne(history, (err, item) => {
             if (err) {
                 console.log(131, err);
                 res.send({ "131 Error is ": + err });
